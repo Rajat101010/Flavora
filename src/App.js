d8855b0151ff970a213f6d538251653f2fb7c28f
Rajat Raj Shah <rajatrajshah@gmail.com>
2025-10-24 13:35:10 +0530

    all

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import dayjs from "dayjs";
 
 import Home from './pages/home';
 import Info from './pages/info';
@@ -14,10 +16,14 @@ import Saturday from './pages/saturday';
 import Sunday from './pages/sunday';
 
 function App() {
+  const day = dayjs().format("dddd").toLowerCase();
+
   return (
     <main style={{ backgroundColor: "#2F2F2F", height: "auto", minHeight:'100vh',  color: "white"}}>
-      <BrowserRouter>
+      <Router>
         <Routes>
+          <Route path="/" element={<Navigate to={`/${day}`} />} />
+
           <Route path='/' element={<Home />} />
           <Route path='/info' element={<Info />} />
           <Route path='/monday' element={<Monday />} />
@@ -28,7 +34,7 @@ function App() {
           <Route path='/saturday' element={<Saturday />} />
           <Route path='/sunday' element={<Sunday />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </main>
   );
 }
